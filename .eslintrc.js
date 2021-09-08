c65a2b9b46ab7ffab6f3cb2228a96202494f4efe
Hunter Marshall <beidah@gmail.com>
2021-09-08 13:36:59 -0500

    Added prettier to eslint and set git hook to lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,9 +3,11 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   plugins: [
     '@typescript-eslint',
+    "prettier"
   ],
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
+    "plugin:prettier/recommended"
   ],
 };
