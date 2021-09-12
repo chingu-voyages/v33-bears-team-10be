902d6a5320999877b56a49ca7de331c7e73d5903
Hunter Marshall <beidah@gmail.com>
2021-09-12 15:03:11 -0500

    Extend User interface for typescript

--- src/index.ts
+++ src/index.ts
@@ -20,21 +20,6 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 
-// Passport session setup.
-//   To support persistent login sessions, Passport needs to be able to
-//   serialize users into and deserialize users out of the session. Typically,
-//   this will be as simple as storing the user ID when serializing, and finding
-//   the user by ID when deserializing. However, since this example does not
-//   have a database of user records, the complete spotify profile is serialized
-//   and deserialized.
-passport.serializeUser(function (user, done) {
-    done(null, user);
-});
-
-passport.deserializeUser(function (obj, done) {
-    done(null, obj);
-});
-
 import { router as authRouter } from './routes/auth';
 
 app.use('/auth', authRouter);
--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -2,6 +2,15 @@ import { Router, Request, Response, NextFunction } from 'express';
 import { Strategy as SpotifyStrategy } from 'passport-spotify';
 import passport from 'passport';
 
+declare global {
+    // eslint-disable-next-line @typescript-eslint/no-namespace
+    namespace Express {
+        interface User {
+            accessToken: string;
+        }
+    }
+}
+
 const router = Router();
 
 const clientID = process.env.CLIENT_ID;
@@ -21,6 +30,21 @@ passport.use(
         },
     ),
 );
+
+// Passport session setup.
+//   To support persistent login sessions, Passport needs to be able to
+//   serialize users into and deserialize users out of the session. Typically,
+//   this will be as simple as storing the user ID when serializing, and finding
+//   the user by ID when deserializing. However, since this example does not
+//   have a database of user records, the complete spotify profile is serialized
+//   and deserialized.
+passport.serializeUser(function (user, done) {
+    done(null, user);
+});
+
+passport.deserializeUser(function (obj: false | Express.User, done) {
+    done(null, obj);
+});
 
 router.get(
     '/',
